b3e3ee490a09969764a3fcc43cb92b5a241d9994
stasyurin <stanislav.unn@gmail.com>
2022-08-04 22:38:33 +0300

    greet

---
+++ src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,13 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+    public static void greet() {
+        System.out.print("May I have your name? ");
+
+        Scanner in = new Scanner(System.in);
+        var name = in.nextLine();
+        System.out.println("Hello, " + name +"!");
+    }
+}
