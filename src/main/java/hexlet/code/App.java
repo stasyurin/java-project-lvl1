c22317d0ba36697b80dd27c7f8aa460de01c020a
stasyurin <stanislav.unn@gmail.com>
2022-08-06 13:55:39 +0300

    greet by "1" enter

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,8 +1,19 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        Cli.greet();
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("0 - Exit");
+
+        System.out.print("Your choice: ");
+        Scanner in = new Scanner(System.in);
+        var noOfGame = Integer.parseInt(in.nextLine());
+        System.out.println();
+        if (noOfGame == 1) {
+            Cli.greet();
+        }
     }
 }
--- src/main/java/hexlet/code/Cli.java
+++ src/main/java/hexlet/code/Cli.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Cli {
     public static void greet() {
+        System.out.println("Welcome to the Brain Games!");
         System.out.print("May I have your name? ");
 
         Scanner in = new Scanner(System.in);
