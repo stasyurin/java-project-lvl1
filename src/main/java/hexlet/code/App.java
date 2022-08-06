fa370f23668ed7deb58c260c6703052931df5c16
stasyurin <stanislav.unn@gmail.com>
2022-08-06 18:04:58 +0300

    Revert "menu loop"
    This reverts commit 3b423157d747a941797f781ddff511d39f219822.

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -4,20 +4,16 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        while (true) {
-            System.out.println("Please enter the game number and press Enter.");
-            System.out.println("1 - Greet");
-            System.out.println("0 - Exit");
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("0 - Exit");
 
-            System.out.print("Your choice: ");
-            Scanner in = new Scanner(System.in);
-            var noOfGame = in.nextLine();
-            System.out.println();
-            if (noOfGame.equals("1")) {
-                Cli.greet();
-            } else if (noOfGame.equals("0")) {
-                break;
-            }
+        System.out.print("Your choice: ");
+        Scanner in = new Scanner(System.in);
+        var noOfGame = in.nextLine();
+        System.out.println();
+        if (noOfGame.equals("1")) {
+            Cli.greet();
         }
     }
 }
