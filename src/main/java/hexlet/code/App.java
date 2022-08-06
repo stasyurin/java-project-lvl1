a14bb3949dceaf586103a30e06c3f744b9239506
stasyurin <stanislav.unn@gmail.com>
2022-08-06 19:10:33 +0300

    Even game add to app

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,19 +1,23 @@
 package hexlet.code;
 
+import hexlet.code.games.Even;
+
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
         System.out.println("Please enter the game number and press Enter.");
         System.out.println("1 - Greet");
+        System.out.println("2 - Even");
         System.out.println("0 - Exit");
 
         System.out.print("Your choice: ");
         Scanner in = new Scanner(System.in);
         var noOfGame = in.nextLine();
         System.out.println();
-        if (noOfGame.equals("1")) {
-            Cli.greet();
+        switch (noOfGame) {
+            case "1" -> Cli.greet();
+            case "2" -> Even.startGame();
         }
     }
 }
