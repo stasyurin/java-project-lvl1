b662c9b0fce8803bb27c667830af6a2eae25aa59
stasyurin <stanislav.unn@gmail.com>
2022-08-25 18:13:59 +0300

    answer and question id

--- src/main/java/hexlet/code/Constants.java
+++ src/main/java/hexlet/code/Constants.java
@@ -5,4 +5,6 @@ public class Constants {
     public static final int LOWER_RANDOM_BOUND = 1;
     public static final int UPPER_RANDOM_BOUND = 101;
     public static final int GAME_INFO_RULES_ID = 0;
+    public static final int ANSWER_AND_QUESTION_QUESTION_ID = 0;
+    public static final int ANSWER_AND_QUESTION_ANSWER_ID = 1;
 }
