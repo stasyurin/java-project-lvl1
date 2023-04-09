5ad011e71c96f647f7072a96dc03e2cd997d315d
stasyurin <stanislav.unn@gmail.com>
2023-04-09 15:03:15 +0300

    negative prime numbers filter

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -24,7 +24,7 @@ public class Prime {
         return questionAnswer;
     }
     static boolean isPrime(int num) {
-        if (num == 1) {
+        if (num < 2) {
             return false;
         }
         for (int i = 2; i <= num / 2; i++) {
