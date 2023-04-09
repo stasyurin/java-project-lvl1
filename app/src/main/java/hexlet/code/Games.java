358d52e9db6e6cf3d52926bbb106d72850291098
stasyurin <stanislav.unn@gmail.com>
2023-04-09 14:40:16 +0300

    delete *-import

--- app/src/main/java/hexlet/code/Games.java
+++ app/src/main/java/hexlet/code/Games.java
@@ -1,6 +1,10 @@
 package hexlet.code;
 
-import hexlet.code.games.*;
+import hexlet.code.games.Calc;
+import hexlet.code.games.Even;
+import hexlet.code.games.GCD;
+import hexlet.code.games.Prime;
+import hexlet.code.games.Progression;
 
 import static hexlet.code.Constants.ANSWER_ID;
 import static hexlet.code.Constants.POINTS_TO_WIN;
