0b855e42500465fc1c822f7423a7e9e9d8a7c212
stasyurin <stanislav.unn@gmail.com>
2023-04-09 12:44:36 +0300

    edge case with 1 is not a prime number

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -23,6 +23,9 @@ public class Prime {
         return questionAnswer;
     }
     static boolean isPrime(int num) {
+        if (num == 1) {
+            return false;
+        }
         for (int i = 2; i <= num / 2; i++) {
             if (num % i == 0) {
                 return false;
