b84cca3b626d20addd2cded8c2f47a630ee43148
stasyurin <stanislav.unn@gmail.com>
2023-04-10 16:04:11 +0300

    OPERATORS array in calc

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -8,33 +8,29 @@ import static hexlet.code.Constants.ANSWER_ID;
 public class Calc {
     static final int LOWER_RANDOM_BOUND = 1;
     static final int UPPER_RANDOM_BOUND = 101;
-    static final int NO_OF_OPERATIONS = 3;
     static final String RULES = "What is the result of the expression?";
+    static final String[] OPERATORS = {"+", "-", "*"};
     public static String rules() {
         return RULES;
     }
     public static String[] questionAnswer() {
-        var operation = RandomUtils.nextInt(0, NO_OF_OPERATIONS);
+        var operation = RandomUtils.nextInt(0, OPERATORS.length - 1);
         var operand1 = RandomUtils.nextInt(LOWER_RANDOM_BOUND, UPPER_RANDOM_BOUND);
         var operand2 = RandomUtils.nextInt(LOWER_RANDOM_BOUND, UPPER_RANDOM_BOUND);
         int operationResult;
-        String operationResultRepresentation;
         switch (operation) {
             default -> {
                 operationResult = operand1 + operand2;
-                operationResultRepresentation = operand1 + " + " + operand2;
             }
             case 1 -> {
                 operationResult = operand1 - operand2;
-                operationResultRepresentation = operand1 + " - " + operand2;
             }
             case 2 -> {
                 operationResult = operand1 * operand2;
-                operationResultRepresentation = operand1 + " * " + operand2;
             }
         }
 
-        var question = operationResultRepresentation;
+        var question = operand1 + " " + OPERATORS[operation] + " " + operand2;
         var answer = Integer.toString(operationResult);
 
         var questionAnswer = new String[2];
