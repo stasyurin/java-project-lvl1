02b22e67eff6475440a301eff8f8652b28e366a2
stasyurin <stanislav.unn@gmail.com>
2023-04-12 16:07:06 +0300

    remove class Games

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,6 +1,11 @@
 package hexlet.code;
 
+import hexlet.code.games.*;
+
 import java.util.Scanner;
+
+import static hexlet.code.Constants.*;
+import static hexlet.code.Constants.ANSWER_ID;
 
 public class App {
     public static void main(String[] args) {
@@ -15,14 +20,67 @@ public class App {
 
         System.out.print("Your choice: ");
         Scanner in = new Scanner(System.in);
-        var input = in.nextLine();
-        if (input.equals("0")) {
+        var noOfGame = in.nextLine();
+        if (noOfGame.equals("0")) {
             return;
-        } else if (input.equals("1")) {
+        } else if (noOfGame.equals("1")) {
             Cli.greet();
             return;
         }
+
         System.out.println();
-        Games.startGame(input);
+        System.out.println("Welcome to the Brain Games!");
+        System.out.print("May I have your name? ");
+        var userName = in.nextLine();
+        System.out.println("Hello, " + userName + "!");
+
+        String rules;
+        switch (noOfGame) {
+            case "2" -> {
+                rules = Even.rules();
+            }
+            case "3" -> {
+                rules = Calc.rules();
+            }
+            case "4" -> {
+                rules = GCD.rules();
+            }
+            case "5" -> {
+                rules = Progression.rules();
+            }
+            case "6" -> {
+                rules = Prime.rules();
+            }
+            default -> {
+                throw new RuntimeException("Unknown game number: " + noOfGame);
+            }
+        }
+        String[][] questionsAnswers = new String[2][POINTS_TO_WIN];
+        for (int i = 0; i < POINTS_TO_WIN; i++) {
+            String[] questionAnswer;
+            switch (noOfGame) {
+                case "2" -> {
+                    questionAnswer = Even.questionAnswer();
+                }
+                case "3" -> {
+                    questionAnswer = Calc.questionAnswer();
+                }
+                case "4" -> {
+                    questionAnswer = GCD.questionAnswer();
+                }
+                case "5" -> {
+                    questionAnswer = Progression.questionAnswer();
+                }
+                case "6" -> {
+                    questionAnswer = Prime.questionAnswer();
+                }
+                default -> {
+                    throw new RuntimeException("Unknown game number: " + noOfGame);
+                }
+            }
+            questionsAnswers[QUESTION_ID][i] = questionAnswer[QUESTION_ID];
+            questionsAnswers[ANSWER_ID][i] = questionAnswer[ANSWER_ID];
+        }
+        Engine.startGame(rules, questionsAnswers, userName);
     }
 }
